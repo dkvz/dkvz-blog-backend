4c39229a5ffa03e0084774ae84523000c0b06a4c
DkVZ <github@dkvz.eu>
2021-02-19 15:18:32 +0100

    IP locator

--- src/stats/ip_location.rs
+++ src/stats/ip_location.rs
@@ -1,6 +1,34 @@
-// Expecting a specific set of Geo data:
+// IP Location module using ip2Location.
+use ip2location::DB;
+use std::net::IpAddr;
+use color_eyre::Result;
+use eyre::{WrapErr, eyre};
 
+// Expecting a specific set of Geo data:
 pub struct GeoInfo {
-  
+  pub country: String,
+  pub region: String,
+  pub city: String
 }
 
+pub struct IpLocator {
+  db: DB
+}
+
+impl IpLocator {
+  
+  pub fn open(filename: &str) -> Result<IpLocator> {
+    match DB::from_file(filename) {
+      Ok(db) => Ok(Self {
+        db
+      }),
+      Err(_) => Err(
+        eyre!("Error opening ip2location DB")
+      )
+    }
+  }
+
+  // Add function to request GeoInfo:
+  
+
+}
