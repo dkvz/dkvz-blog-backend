315c91f37a05158869c8a3de70250db3afce0d0a
dkvz <github@dkvz.eu>
2026-04-16 11:24:17 +0200

    fix another unparsable log line

--- src/log-to-stats.rs
+++ src/log-to-stats.rs
@@ -340,7 +340,7 @@ fn parse_log_line(line: &str, url_parser: &UrlParser) -> Result<Option<ParsedLog
         // because some requests do not have any verb and still get
         // logged for some reason. Might be an Nginx thing.
         static ref RE_LOG_LINE: Regex =
-            Regex::new(r#"^(\S+?)\s-.+\[(.+?)\]\s"\S{0,5}\s?(\S+?)(\s.+?)?"\s(\d+)\s.+?"(\S+?)"\s"(.+)"$"#).unwrap();
+            Regex::new(r#"^(\S+?)\s-.+\[(.+?)\]\s"\S{0,5}\s?(\S*?)(\s.+?)?"\s(\d+)\s.+?"(\S+?)"\s"(.+)"$"#).unwrap();
     }
 
     let captures = RE_LOG_LINE.captures(line);
@@ -467,6 +467,14 @@ mod log_to_stats_tests {
         let parsed1 = parse_log_line(line1, &url_parser).unwrap();
         let parsed2 = parse_log_line(line2, &url_parser).unwrap();
         assert!(parsed1.is_none() && parsed2.is_none());
+    }
+
+    #[test]
+    fn can_parse_log_line_empty_url() {
+        let line = r###"3.2.241.100 - - [10/Apr/2026:20:44:03 +0200] "" 400 0 "-" "-""###;
+        let url_parser = UrlParser::from("articles", "breves").unwrap();
+        let parsed = parse_log_line(line, &url_parser).unwrap();
+        assert!(parsed.is_none());
     }
 
     #[test]
